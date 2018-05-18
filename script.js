93ee4183e60270029cdc535e2d972100a4bf2a97
Mozillex <Mozillex@users.noreply.github.com>
2018-05-18 11:17:59 -0400

    Final version, as published today to https://codepen.io/Mozillex/full/GdPZzm

--- script.js
+++ script.js
@@ -1,177 +1,162 @@
-let origBoard;
-let p1;
-let p2;
-const winSets = [
-	[0, 1, 2],
-	[3, 4, 5],
-	[6, 7, 8],
-	[0, 3, 6],
-	[1, 4, 7],
-	[2, 5, 8],
-	[0, 4, 8],
-	[6, 4, 2]
-]
-count = 0;
-const squares = document.querySelectorAll('div.square');
-
-squares.forEach(sq => sq.id = '' + count++);
+let positions;
+let  p1 = 'X';
+let  p2 = 'O';
 
 const popup = document.getElementById('popup');
-const chooseMark = document.querySelectorAll('#popup>div');
-let firstUp = Math.floor(Math.random()*2) ? 'p1' : 'p2';
+const msg = document.getElementById('msg');
+const end = document.getElementById('end');
+const endMsg = document.getElementById('endMsg');
+const backEnd = document.getElementById('backEnd');
+const choice = document.getElementById('choice');
 
-let startGame = function startGame(r) {
-  if (r) console.log(r);
-  count = 0;
-  let first = firstUp;
+const winSets =[
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [6, 4, 2],
+  [2, 5, 8],
+  [1, 4, 7],
+  [0, 3, 6]
+];
 
-  chooseMark.forEach(e => {
-  	e.onclick = () => {
-  		p1 = e.textContent;
-  		p2 = (e.textContent === 'X' ? 'O' : 'X');
-  		popup.className = 'step2';
-  		popup.textContent = 'Good luck, Player1!';
+const squares = document.querySelectorAll('.square');
 
-  		function whoIsFirst(){
-  			popup.textContent = (first === 'p1' ? "You're " : p2 + ' is') + ' up first.';
-  			setTimeout(closePopUp, 1500);
-  		}
+function chooseMark(mark){
+  p1 = mark;
+  p2 = (mark === 'X' ? 'O' :'X');
+  positions = Array.from(Array(9).keys());
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', p1Move, false);
+    squares[i].textContent = '';
+  }
+  if (p2 === 'X') {
+    takeTurn(bestSpot(), p2);
+  }
+  choice.style.display = 'none';
+  popup.style.display = 'none';
+}
 
-  		function closePopUp(){
-  			popup.style.display = 'none';
-        goOn();
-  		}
-  		setTimeout(whoIsFirst, 1500);
-  	};
-  });
+function startGame() {
+  end.style.display = 'none';
+  choice.style.display = 'block';
+  popup.style.display = 'block';
+  squares.forEach(x => x.className = 'square');
+}
 
-  function goOn(){
-
-  	origBoard = Array.from(Array(9).keys());
-  	for (let i = 0; i < squares.length; i++) {
-  		squares[i].innerText = '';
-  		squares[i].classList.remove('win');
-      squares[i].style.color = 'white';
-  		squares[i].addEventListener('click', p1Move);
-  	}
-    if(first === 'p2') makeMove('4', p2);
+function p1Move(pos) {
+  if (typeof positions[pos.target.id] ==='number') {
+    takeTurn(pos.target.id, p1);
+    if (!checkWin(positions, p1) && !checkTie()) takeTurn(bestSpot(), p2);
   }
 }
 
-function p1Move(square) {
-	if (typeof origBoard[square.target.id] == 'number') {
-		makeMove(square.target.id, p1)
-		if (!checkWin(origBoard, p1) && !tieGame()) makeMove(bestSpot(), p2);
-	}
-}
-
-function makeMove(squareId, player) {
-	origBoard[squareId] = player;
-	document.getElementById(squareId).innerText = player;
-	let gameWon = checkWin(origBoard, player)
-	if (gameWon) gameOver(gameWon)
+function takeTurn(squareId, player) {
+  positions[squareId] = player;
+  document.getElementById(squareId).innerHTML = player;
+  let  winGame = checkWin(positions, player);
+  if (winGame) gameOver(winGame);
+  checkTie();
 }
 
 function checkWin(board, player) {
-	let plays = board.reduce((a, e, i) =>
-		(e === player) ? a.concat(i) : a, []);
-	let gameWon = null;
-	for (let [index, win] of winSets.entries()) {
-		if (win.every(elem => plays.indexOf(elem) > -1)) {
-			gameWon = {index: index, player: player};
-			break;
-		}
-	}
-	return gameWon;
+  let plays = board.reduce((a, e, i) => (e === player) ? a.concat(i) : a, []);
+  let  winner = null;
+  for (let [index, win] of winSets.entries()) {
+    if (win.every(elem => plays.indexOf(elem) > -1)) {
+       winner = {index: index, player: player};
+      break;
+    }
+  }
+  return  winner;
 }
 
-function gameOver(gameWon) {
-	for (let index of winSets[gameWon.index]) {
-		document.getElementById(index).className += ' win';
-	}
-	for (let i = 0; i < squares.length; i++) {
-		squares[i].removeEventListener('click', p1Move);
-	}
-	declareWinner(gameWon.player == p1 ? "You win!" : "You lose.");
-  //setTimeout(startGame, 2000);
+function gameOver(winner){
+  for (let index of winSets[winner.index]) document.getElementById(index).classList.add('win');
+  for (let i=0; i < squares.length; i++) {
+    squares[i].removeEventListener('click', p1Move, false);
+  }
+  declareWinner((winner.player ===  p1 ? "You win!" : winner.player + " wins. Try Again."),winner.player);
+}
+
+let repeat;
+function again(){
   startGame();
+  chooseMark(p1);
 }
 
-function declareWinner(msg) {
-	document.querySelector("#popup").style.display = "block";
-	document.querySelector("#popup").textContent = msg;
-  setTimeout(startGame, 2000);
-
+function declareWinner(message,player) {
+  end.style.display = "block";
+  backEnd.textContent = player;
+  endMsg.textContent = message;
+  repeat = setTimeout(again, 2000);
 }
 
-function emptySquares() {
-	return origBoard.filter(s => typeof s == 'number');
+function openSquares() {
+  return positions.filter((x, y) => y === x);
 }
 
-function bestSpot() {
-	return minimax(origBoard, p2).index;
+function bestSpot(){
+  return minMax(positions, p2).index;
 }
 
-function tieGame() {
-	if (emptySquares().length == 0) {
-		for (let i = 0; i < squares.length; i++) {
-			squares[i].style.color = "grey";
-			squares[i].removeEventListener('click', p1Move);
-		}
-		declareWinner("Tie Game!")
-		return true;
-	}
-	return false;
+function checkTie() {
+  if (openSquares().length === 0){
+    for(let i = 0; i< squares.length; i++) {
+    squares[i].style.color = '#757575';
+    squares[i].removeEventListener('click',p1Move, false);
+    };
+    declareWinner("Tie game");
+    return true;
+  }
+  return false;
 }
 
-function minimax(newBoard, player) {
-	let availSpots = emptySquares();
+function minMax(newBoard, player) {
+  let openSq = openSquares(newBoard);
 
-	if (checkWin(newBoard, p1)) {
-		return {score: -10};
-	} else if (checkWin(newBoard, p2)) {
-		return {score: 10};
-	} else if (availSpots.length === 0) {
-		return {score: 0};
-	}
-	let moves = [];
-	for (let i = 0; i < availSpots.length; i++) {
-		let move = {};
-		move.index = newBoard[availSpots[i]];
-		newBoard[availSpots[i]] = player;
+  if (checkWin(newBoard, p1)) {
+    return {score: -10};
+  } else if (checkWin(newBoard, p2)) {
+    return {score: 10};
+  } else if (openSq.length === 0) {
+    return {score: 0};
+  }
+  let moves = [];
+  for (let i = 0; i < openSq.length; i ++) {
+    let move = {};
+    move.index = newBoard[openSq[i]];
+    newBoard[openSq[i]] = player;
 
-		if (player == p2) {
-			let result = minimax(newBoard, p1);
-			move.score = result.score;
-		} else {
-			let result = minimax(newBoard, p2);
-			move.score = result.score;
-		}
+    if (player === p2)
+      move.score = minMax(newBoard, p1).score;
+    else
+       move.score =  minMax(newBoard, p2).score;
+    newBoard[openSq[i]] = move.index;
+    if ((player === p2 && move.score === 10) || (player === p1 && move.score === -10))
+      return move;
+    else
+      moves.push(move);
+  }
 
-		newBoard[availSpots[i]] = move.index;
-
-		moves.push(move);
-	}
-
-	let bestMove;
-	if(player === p2) {
-		let bestScore = -10000;
-		for(let i = 0; i < moves.length; i++) {
-			if (moves[i].score > bestScore) {
-				bestScore = moves[i].score;
-				bestMove = i;
-			}
-		}
-	} else {
-		let bestScore = 10000;
-		for(let i = 0; i < moves.length; i++) {
-			if (moves[i].score < bestScore) {
-				bestScore = moves[i].score;
-				bestMove = i;
-			}
-		}
-	}
-
-	return moves[bestMove];
+  let bestMove, bestScore;
+  if (player === p2) {
+    bestScore = -1000;
+    for(let i = 0; i < moves.length; i++) {
+      if (moves[i].score > bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  } else {
+      bestScore = 1000;
+      for(let i = 0; i < moves.length; i++) {
+      if (moves[i].score < bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  }
+  return moves[bestMove];
 }
 startGame();
